188fe0c0e2b8cda3dd23a7413cc48be3cdd78a63
Sirjaime007 <93615006+Sirjaime007@users.noreply.github.com>
2026-05-05 15:00:25 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'v1-cafe-cache';
+const assets = [
+  'index.html',
+  'manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
